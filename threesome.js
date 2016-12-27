b8f5df745b23844cde5caa05845655e91b112e6e
hcz <h.cz@qq.com>
2016-12-27 23:29:33 +0800

    init

---
+++ threesome.js
@@ -0,0 +1,309 @@
+'use strict';
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = require('./token').threesome;
+
+process.on('uncaughtException', (err) => {
+    console.err(err);
+});
+
+const bot = new TelegramBot(token, {
+    polling: {
+        interval: 1000,
+    },
+});
+
+const games = {};
+
+const join = (msg, match) => {
+    const game = games[msg.chat.id];
+
+    if (!game.users[msg.from.id] && game.usercount < game.modemax) {
+        game.usercount += 1;
+        game.users[msg.from.id] = true;
+
+        if (game.time > -60) {
+            game.time = -60;
+        }
+
+        bot.sendMessage(
+            msg.chat.id,
+            (msg.from.first_name || msg.from.last_name) + ' 加入了' + game.modename + '，'
+                + game.usercount + ' 名禽兽参加，'
+                + '最少 ' + game.modemin + ' 人参加，'
+                + '最多 ' + game.modemax + ' 人参加'
+        ).then(() => {
+            if (game.usercount === game.modemax) {
+                game.time = 0;
+            }
+        });
+    }
+};
+
+const flee = (msg, match) => {
+    const game = games[msg.chat.id];
+
+    if (game.users[msg.from.id]) {
+        game.usercount -= 1;
+        delete game.users[msg.from.id];
+
+        if (game.time > -30) {
+            game.time = -30;
+        }
+
+        bot.sendMessage(
+            msg.chat.id,
+            (msg.from.first_name || msg.from.last_name) + ' 逃离了' + game.modename + '，'
+                + '不再与大家啪啪\n\n'
+                + '剩余 ' + game.usercount + ' 人'
+        );
+    }
+};
+
+const start = (msg, match) => {
+    const game = games[msg.chat.id];
+    console.log(game);
+
+    bot.sendMessage(
+        msg.chat.id,
+        '开始啪啪啦！啪啪啪啪啪啪啪啪'
+    );
+};
+
+const finish = (msg, match) => {
+    const game = games[msg.chat.id];
+
+    bot.sendMessage(
+        msg.chat.id,
+        '啪啪结束'
+    );
+
+    delete games[msg.chat.id];
+};
+
+const cancel = (msg, match) => {
+    const game = games[msg.chat.id];
+
+    bot.sendMessage(
+        msg.chat.id,
+        '禽兽人数不足，已取消' + game.modename
+    );
+
+    delete games[msg.chat.id];
+};
+
+const na = (msg, match) => {
+    bot.sendMessage(
+        msg.chat.id,
+        '目前没有床上运动进行中，\n'
+            + '/startmasturbate 启动一场撸管\n'
+            + '/startsex 启动一场啪啪\n'
+            + '/startthreesome 启动 3P 模式'
+    );
+};
+
+bot.onText(/^\/nextsex/, (msg, match) => {
+    bot.sendMessage(
+        msg.chat.id,
+        '我不会通知你的，请洗干净自己来'
+    );
+});
+
+bot.onText(/^\/startmasturbate/, (msg, match) => {
+    if (games[msg.chat.id]) {
+        const game = games[msg.chat.id];
+
+        if (game.time <= 0) {
+            join(msg, match);
+        }
+    } else {
+        games[msg.chat.id] = {
+            usercount: 0,
+            users: {},
+            modename: '撸管',
+            modemin: 1,
+            modemax: 1,
+            time: -300,
+        };
+
+        bot.sendMessage(
+            msg.chat.id,
+            (msg.from.first_name || msg.from.last_name) + ' 决定自己撸一炮！',
+            {
+                reply_to_message_id: msg.message_id,
+            }
+        ).then(() => {
+            join(msg, match);
+        });
+    }
+});
+
+bot.onText(/^\/startsex/, (msg, match) => {
+    if (games[msg.chat.id]) {
+        const game = games[msg.chat.id];
+
+        if (game.time <= 0) {
+            join(msg, match);
+        }
+    } else {
+        games[msg.chat.id] = {
+            usercount: 0,
+            users: {},
+            modename: '滚床单活动',
+            modemin: 2,
+            modemax: 2,
+            time: -300,
+        };
+
+        bot.sendMessage(
+            msg.chat.id,
+            (msg.from.first_name || msg.from.last_name) + ' 已经启动了一场约炮！'
+                + '输入指令 /join 来参加这场床单盛宴...'
+                + '不排除有怀孕的可能 :P',
+            {
+                reply_to_message_id: msg.message_id,
+            }
+        ).then(() => {
+            join(msg, match);
+        });
+    }
+});
+
+bot.onText(/^\/startthreesome/, (msg, match) => {
+    if (games[msg.chat.id]) {
+        const game = games[msg.chat.id];
+
+        if (game.time <= 0) {
+            join(msg, match);
+        }
+    } else {
+        games[msg.chat.id] = {
+            usercount: 0,
+            users: {},
+            modename: '这场 3P',
+            modemin: 3,
+            modemax: 3,
+            time: -300,
+        };
+
+        bot.sendMessage(
+            msg.chat.id,
+            '在 ' + (msg.from.first_name || '') + ' 的带领下，'
+                + '3P 模式正式启动！'
+                + '输入 /join，群里的禽兽们将会全面进场！',
+            {
+                reply_to_message_id: msg.message_id,
+            }
+        ).then(() => {
+            join(msg, match);
+        });
+    }
+});
+
+bot.onText(/^\/join/, (msg, match) => {
+    if (games[msg.chat.id]) {
+        const game = games[msg.chat.id];
+
+        if (game.time <= 0) {
+            join(msg, match);
+        } else {
+            // TODO: check user
+            if (!game.users[msg.from.id]) {
+                bot.sendMessage(
+                    msg.chat.id,
+                    (msg.from.first_name || msg.from.last_name) + ' 按耐不住，'
+                        + '强行插入了这场' + game.modename
+                );
+            }
+        }
+    } else {
+        na(msg, match);
+    }
+});
+
+bot.onText(/^\/flee/, (msg, match) => {
+    if (games[msg.chat.id]) {
+        const game = games[msg.chat.id];
+
+        if (game.time <= 0) {
+            flee(msg, match);
+        } else {
+            bot.sendMessage(
+                msg.chat.id,
+                (msg.from.first_name || msg.from.last_name) + ' 拔了出来，'
+                    + '然后忍不住又插了进去，'
+                    + '回到了' + game.modename
+            );
+        }
+    } else {
+        na(msg, match);
+    }
+});
+
+bot.onText(/^\/forcestart/, (msg, match) => {
+    if (games[msg.chat.id]) {
+        const game = games[msg.chat.id];
+
+        if (game.time <= 0) {
+            game.time = 0;
+        }
+    } else {
+        na(msg, match);
+    }
+});
+
+setInterval(() => {
+    for (const i in games) {
+        const game = games[i];
+
+        switch (game.time) {
+            case -60:
+                bot.sendMessage(
+                    i,
+                    '剩余一分钟 /join'
+                );
+
+                break;
+            case -30:
+                bot.sendMessage(
+                    i,
+                    '剩余 30 秒 /join'
+                );
+
+                break;
+            case -10:
+                bot.sendMessage(
+                    i,
+                    '剩余 10 秒 /join'
+                );
+
+                break;
+            case 0:
+                // mock objects
+
+                if (game.usercount >= game.modemin) {
+                    start({
+                        chat: {
+                            id: i,
+                        }
+                    }, []);
+                } else {
+                    cancel({
+                        chat: {
+                            id: i,
+                        }
+                    }, []);
+                }
+
+                break;
+            case 60:
+                finish({
+                    chat: {
+                        id: i,
+                    }
+                }, []);
+        }
+
+        game.time += 1;
+    }
+}, 900);
---
+++ werewoof.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = require('./token').werewoof;
+const group = -1001073512575;
+const botname = 'werewoofbot';
+
+process.on('uncaughtException', (err) => {
+    console.err(err);
+});
+
+const bot = new TelegramBot(token, {
+    polling: {
+        interval: 1000,
+    },
+});
+
+const handler = (func) => {
+    return (msg, match) => {
+        console.log(JSON.stringify(msg));
+
+        if (msg.date >= Date.now() / 1000 - 10) {
+            if (msg.text) {
+                func(msg, match);
+
+                msg.text = null;
+            }
+        }
+    };
+};
+
+const messages = [
+    [/./, '神tm#MSG', 0.05, true],
+    [/test/, '喵喵喵～', 1, true],
+    [/\/start/, '来吸毒啊！', 0.25, false],
+    [/\/join/, '快/flee！', 0.1, true],
+    [/\/unite/, '我们为什么不能合体！', 1, false],
+    [/神tm/, '别学我！', 0.5, true],
+    [/插/, '拔出来！', 0.5, true],
+    [/^[投出票]/, '投好庄严一票', 0.2, false],
+    [/^[投出票]/, '出先知', 0.25, false],
+    [/^[跳]/, '跳大神', 0.2, false],
+    [/^[跳]/, '跳迪斯科', 0.25, false],
+    [/[鸡机基积级集几Jj🐔][巴八吧扒把Bb8]/, '说鸡不说巴，文明你我他', 0.8, true],
+    [/[鸡机基积级集几Jj🐔][巴八吧扒把Bb8]/, '说鸡就说巴，文明去他妈', 1, true],
+];
+
+for (const i in messages) {
+    ((message) => {
+        if (Math.random() < messages[i][2]) {
+            bot.onText(message[0], handler((msg, match) => {
+                bot.sendMessage(
+                    group,
+                    message[1].replace('#MSG', msg.text),
+                    {
+                        reply_to_message_id: message[3] ? msg.message_id : null,
+                        parse_mode: 'HTML',
+                    }
+                );
+            }));
+        }
+    })(messages[i]);
+}
+
+bot.onText(/./, handler((msg, match) => {
+    if (msg.chat.id === group) {
+        if (
+            msg.reply_to_message
+            && msg.reply_to_message.from.username === botname
+        ) {
+            if (Math.random() < 0.3) {
+                bot.sendMessage(
+                    group,
+                    '么么哒～',
+                    {
+                        reply_to_message_id: msg.message_id,
+                        parse_mode: 'HTML',
+                    }
+                );
+            } if (Math.random() < 0.3) {
+                bot.sendMessage(
+                    group,
+                    'cao你妈～',
+                    {
+                        reply_to_message_id: msg.message_id,
+                        parse_mode: 'HTML',
+                    }
+                );
+            } else {
+                bot.sendMessage(
+                    group,
+                    '神tm ' + msg.text,
+                    {
+                        reply_to_message_id: msg.message_id,
+                        parse_mode: 'HTML',
+                    }
+                );
+            }
+        }
+    } else {
+        bot.sendMessage(
+            group,
+            msg.text,
+            {
+                parse_mode: 'HTML',
+            }
+        );
+    }
+}));
+
+bot.on('message', (msg) => {
+    if (msg.new_chat_member) {
+        bot.sendMessage(
+            group,
+            '让我们祝福这对新人！',
+            {
+                parse_mode: 'HTML',
+            }
+        );
+    }
+});
