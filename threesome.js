80ab5e281cb01f80cbe99d75dcde491529defba6
hcz <h.cz@qq.com>
2017-06-25 22:59:26 +0800

    add threesome.init.js

--- threesome.js
+++ threesome.js
@@ -7,6 +7,8 @@ const data = require('./threesome.data')(config.threesomePathActions, config.thr
 
 const info = require('./threesome.info')(bot);
 const gather = require('./threesome.gather')(bot, data.games);
+const init = require('./threesome.init')(bot, data.games);
+
 process.on('uncaughtException', (err) => {
     console.error(err);
 });
@@ -90,23 +92,8 @@ bot.onText(/^\/startmasturbate/, event((msg, match) => {
             gather.join(msg);
         }
     } else {
-        data.games[msg.chat.id] = {
-            usercount: 0,
-            users: {},
-            modename: '撸管',
-            modemin: 1,
-            modemax: 1,
-            time: -120,
-        };
-
-        bot.sendMessage(
-            msg.chat.id,
-            (msg.from.first_name || msg.from.last_name) + ' 决定自己撸一炮！',
-            {
-                reply_to_message_id: msg.message_id,
-            }
-        ).then(() => {
-            join(msg, match);
+        init.masturbate(msg).then(() => {
+            gather.join(msg);
         });
     }
 }));
@@ -119,25 +106,8 @@ bot.onText(/^\/startsex/, event((msg, match) => {
             gather.join(msg);
         }
     } else {
-        data.games[msg.chat.id] = {
-            usercount: 0,
-            users: {},
-            modename: '滚床单活动',
-            modemin: 2,
-            modemax: 2,
-            time: -180,
-        };
-
-        bot.sendMessage(
-            msg.chat.id,
-            (msg.from.first_name || msg.from.last_name) + ' 已经启动了一场约炮！'
-                + '输入指令 /join 来参加这场床单盛宴...'
-                + '不排除有怀孕的可能 :P',
-            {
-                reply_to_message_id: msg.message_id,
-            }
-        ).then(() => {
-            join(msg, match);
+        init.sex(msg).then(() => {
+            gather.join(msg);
         });
     }
 }));
@@ -150,25 +120,8 @@ bot.onText(/^\/startthreesome/, event((msg, match) => {
             gather.join(msg);
         }
     } else {
-        data.games[msg.chat.id] = {
-            usercount: 0,
-            users: {},
-            modename: '这场 3P',
-            modemin: 3,
-            modemax: 3,
-            time: -300,
-        };
-
-        bot.sendMessage(
-            msg.chat.id,
-            '在 ' + (msg.from.first_name || '') + ' 的带领下，'
-                + '3P 模式正式启动！'
-                + '输入 /join，群里的禽兽们将会全面进场！',
-            {
-                reply_to_message_id: msg.message_id,
-            }
-        ).then(() => {
-            join(msg, match);
+        init.threesome(msg).then(() => {
+            gather.join(msg);
         });
     }
 }));
@@ -181,25 +134,8 @@ bot.onText(/^\/startgroupsex/, event((msg, match) => {
             gather.join(msg);
         }
     } else {
-        data.games[msg.chat.id] = {
-            usercount: 0,
-            users: {},
-            modename: '这场群P',
-            modemin: 3,
-            modemax: 100,
-            time: -300,
-        };
-
-        bot.sendMessage(
-            msg.chat.id,
-            '在 ' + (msg.from.first_name || '') + ' 的带领下，'
-                + '群P 模式正式启动！'
-                + '输入 /join，群里的禽兽们将会全面进场！',
-            {
-                reply_to_message_id: msg.message_id,
-            }
-        ).then(() => {
-            join(msg, match);
+        init.groupsex(msg).then(() => {
+            gather.join(msg);
         });
     }
 }));
@@ -212,25 +148,8 @@ bot.onText(/^\/start100kills/, event((msg, match) => {
             gather.join(msg);
         }
     } else {
-        data.games[msg.chat.id] = {
-            usercount: 0,
-            users: {},
-            modename: '百人斩',
-            modemin: 100,
-            modemax: 100,
-            time: -300,
-        };
-
-        bot.sendMessage(
-            msg.chat.id,
-            '在 ' + (msg.from.first_name || '') + ' 的带领下，'
-                + '百人斩模式正式启动！'
-                + '输入 /join，成为这场豪华盛宴的百分之一！',
-            {
-                reply_to_message_id: msg.message_id,
-            }
-        ).then(() => {
-            join(msg, match);
+        init.kills(msg).then(() => {
+            gather.join(msg);
         });
     }
 }));
