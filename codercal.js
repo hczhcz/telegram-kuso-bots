a55a927e1ceef80a6d23c1eeb63f1eed9bfdd8b1
hcz <h.cz@qq.com>
2017-08-31 00:31:06 -0400

    add commands

--- codercal.js
+++ codercal.js
@@ -11,6 +11,48 @@ process.on('uncaughtException', (err) => {
     console.error(err);
 });
 
+// /calender <cal id> <title>
+bot.onText(/^\/(calender|c) (\w+cal) ([^@\r\n]+)/, handleAction);
+// /delete calender <cal id>
+bot.onText(/^\/(delete calender|dc) (\w+cal)/, handleAction);
+
+// /dictionary <cal id> <comp id> <random>
+// /dictionary <cal id> <comp id> x<pick>
+bot.onText(/^\/(dictionary|d) (\w+cal) (\w+) (x?)(\d+)/, handleAction);
+
+// /item <cal id> <comp id> <text>
+bot.onText(/^\/(item|i) (\w+cal) (\w+) ([^@\r\n]+)/, handleAction);
+// /delete item <cal id> <comp id> <text>
+bot.onText(/^\/(delete item|di) (\w+cal) (\w+) ([^@\r\n]+)/, handleAction);
+
+// /activity <cal id> <name>@<good>@<bad>
+// /activity <cal id> <name>@<good>@<bad>@weekday
+// /activity <cal id> <name>@<good>@<bad>@weekend
+bot.onText(/^\/(activity|a) (\w+cal) ([^@\r\n]+)@([^@\r\n]*)@([^@\r\n]*)(@weekday|@weekend)?/, handleAction);
+// /delete activity <cal id> <name>
+bot.onText(/^\/(delete activity|da) (\w+cal) ([^@\r\n]+)/, handleAction);
+
+// /special <cal id> <name>@good@<good>@<date>
+// /special <cal id> <name>@bad@<bad>@<date>
+bot.onText(/^\/(special|s) (\w+cal) ([^@\r\n]+)@(good|bad)@([^@\r\n]*)@(\d+)/, handleAction);
+// /delete special <cal id> <name>
+bot.onText(/^\/(delete special|ds) (\w+cal) ([^@\r\n]+)/, handleAction);
+
+// /hint <cal id> <text>
+bot.onText(/^\/(hint|h) (\w+cal) ([^@\r\n]+)/, handleAction);
+// /delete hint <cal id> <text>
+bot.onText(/^\/(delete hint|dh) (\w+cal) ([^@\r\n]+)/, handleAction);
+
+// /luck <luck id> <title>@<random>
+bot.onText(/^\/(luck|l) (\w+luck) ([^@\r\n]+)@(\d+)/, handleAction);
+// /delete luck <luck id>
+bot.onText(/^\/(delete luck|dl) (\w+luck)/, handleAction);
+
+// /rate <luck id> <name>@<rate>@<descrpiton>
+bot.onText(/^\/(rate|r) (\w+luck) ([^@\r\n]+)@(\d+)@([^@\r\n]*)/, handleAction);
+// /delete rate <luck id> <name>
+bot.onText(/^\/(delete rate|dr) (\w+luck) ([^@\r\n]+)/, handleAction);
+
 bot.on('inline_query', (query) => {
     if (config.ban[query.from.id]) {
         return bot.answerInlineQuery(query.id, [{
