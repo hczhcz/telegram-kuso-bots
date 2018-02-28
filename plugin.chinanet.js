422ce41ebae8bbf96cd423d4992d5b2dc1236f92
hcz <h.cz@qq.com>
2018-02-28 00:13:21 -0500

    remove log.chinanet

--- plugin.chinanet.js
+++ plugin.chinanet.js
@@ -1,20 +1,10 @@
 'use strict';
 
-const fs = require('fs');
-
 const config = require('./config');
 
 module.exports = (bot, event, playerEvent, env) => {
-    const fd = fs.openSync('log.chinanet', 'a');
-
     const genEvent = (key) => {
         return (msg, match) => {
-            fs.write(fd, JSON.stringify({
-                msg: msg,
-            }) + '\n', () => {
-                // nothing
-            });
-
             env.command.get(msg, key, []);
         };
     };
