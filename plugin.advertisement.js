e1321936f2d9a7dccce5e0ec54812803609781d1
hcz <h.cz@qq.com>
2018-03-14 23:44:15 -0400

    implement as zao

--- plugin.advertisement.js
+++ plugin.advertisement.js
@@ -1,30 +1,48 @@
 'use strict';
 
+const fs = require('fs');
+
 const config = require('./config');
 
 module.exports = (bot, event, playerEvent, env) => {
+    const fd = fs.openSync('log.advertisement', 'a');
+
     const ad = [];
 
-    bot.onText(/^\/ad(@\w+)?(?: ([^\r\n]*))?$/, event((msg, match) => {
+    bot.onText(/^\/zao(@\w+)?(?: ([^\r\n]*))?$/, event((msg, match) => {
         if (match[2] && match[2].length <= config.advertisementMaxLength) {
-            for (const i in ad) {
-                if (ad[i].from.id === msg.from.id) {
-                    ad.splice(i, 1);
-                }
-            }
-
-            ad.unshift({
-                from: msg.from,
-                text: match[2],
+            ad.filter((item, i, arr) => {
+                return item.from.id !== msg.from.id;
             });
 
-            ad.splice(config.advertisementCount);
-        }
+            const now = new Date();
+            const localTime = new Date(now.getTime() + (now.getTimezoneOffset() + 240) * 60 * 1000);
+            const time = localTime.getUTCHours() + ':' + localTime.getUTCMinutes();
 
+            const obj = {
+                from: msg.from,
+                time: time,
+                text: match[2],
+            };
+
+            fs.write(fd, JSON.stringify(obj) + '\n', () => {
+                // nothing
+            });
+
+            ad.push(obj);
+
+            if (ad.length >= config.advertisementCount) {
+                ad.shift();
+            }
+        }
+    }, 1));
+
+    bot.onText(/^\/zaog[au]ys(@\w+)?(?: ([^\r\n]*))?$/, event((msg, match) => {
         let result = '';
 
         for (const i in ad) {
-            result += (ad[i].from.username || ad[i].from.first_name) + ': '
+            result += (ad[i].from.username || ad[i].from.first_name) + ' '
+                + ad[i].time + ' '
                 + ad[i].text + '\n';
         }
 
