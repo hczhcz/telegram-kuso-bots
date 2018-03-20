88caa4e8f8d344986379837b0670bb340f70c2c9
hcz <h.cz@qq.com>
2018-03-20 02:30:26 -0400

    add minesweeper.core.js

---
+++ minesweeper.core.js
@@ -0,0 +1,154 @@
+'use strict';
+
+// tags
+// s = space
+// S = space with flag
+// m = mine
+// M = mine with flag
+// 0-8 = opened space
+// * = opened mine
+
+const flag = (map, targetI, targetJ) => {
+    switch (map[targetI][targetJ]) {
+        case 'S':
+            map[targetI][targetJ] = 's';
+
+            break;
+        case 's':
+            map[targetI][targetJ] = 'S';
+
+            break;
+        case 'M':
+            map[targetI][targetJ] = 'm';
+
+            break;
+        case 'm':
+            map[targetI][targetJ] = 'M';
+
+            break;
+        default:
+            // ignore
+    }
+};
+
+const click = (map, targetI, targetJ) => {
+    const scan = (callback) => {
+        for (let i = targetI - 1; i <= targetI + 1; i += 1) {
+            for (let j = targetJ - 1; j <= targetJ + 1; j += 1) {
+                if (
+                    (i !== targetI || j !== targetJ)
+                    && i >= 0 && i < map.length
+                    && j >= 0 && j < map[0].length
+                ) {
+                    callback(i, j, map[i][j]);
+                }
+            }
+        }
+    };
+
+    if (map[targetI][targetJ] === 's') {
+        let nearSpace = 0;
+        let nearMine = 0;
+
+        scan((i, j, value) => {
+            if (value === 'S' || value === 's') {
+                nearSpace += 1;
+            } else if (value === 'M' || value === 'm' || value === '*') {
+                nearMine += 1;
+            }
+        });
+
+        map[targetI][targetJ] = nearMine;
+
+        if (nearSpace === 0) {
+            // TODO: option?
+
+            scan((i, j, value) => {
+                if (value === 'm') {
+                    flag(map, i, j);
+                }
+            });
+        }
+
+        if (nearMine === 0) {
+            scan((i, j, value) => {
+                if (value === 'S' || value === 's') {
+                    click(map, i, j);
+                }
+            });
+        }
+    } else if (map[targetI][targetJ] === 'm') {
+        map[targetI][targetJ] = '*';
+    } else if (map[targetI][targetJ] >= 0 && map[targetI][targetJ] <= 8) {
+        // TODO: option?
+
+        let nearFlag = 0;
+
+        scan((i, j, value) => {
+            if (value === 'S' || value === 'M') {
+                nearFlag += 1;
+            }
+        });
+
+        if (nearFlag === map[targetI][targetJ]) {
+            scan((i, j, value) => {
+                if (value === 's' || value === 'm') {
+                    click(map, i, j);
+                }
+            });
+        }
+    }
+};
+
+const verify = (rows, columns, mines) => {
+    return rows > 0 && columns > 0 && mines > 0
+        && rows <= 16 && columns <= 8 && mines <= rows * columns - 1;
+};
+
+const init = (rows, columns, mines, targetI, targetJ) => {
+    const map = [];
+
+    let space = rows * columns - 1;
+    let remain = mines;
+
+    for (let i = 0; i < rows; i += 1) {
+        map.push([]);
+
+        for (let j = 0; j < columns; j += 1) {
+            if (i === targetI && j === targetJ) {
+                map[i].push('s');
+            } else if (Math.random() * space < remain) {
+                map[i].push('m');
+                space -= 1;
+                remain -= 1;
+            } else {
+                map[i].push('s');
+                space -= 1;
+            }
+        }
+    }
+
+    return map;
+};
+
+const status = (map) => {
+    for (let i = 0; i < map.length; i += 1) {
+        for (let j = 0; j < map[0].length; j += 1) {
+            if (map[i][j] === 'S' || map[i][j] === 's') {
+                return 'normal';
+            } else if (map[i][j] === '*') {
+                return 'lose';
+            }
+        }
+    }
+
+    return 'win';
+};
+
+module.exports = {
+    click: click,
+    flag: flag,
+    verify: verify,
+    init: init,
+    status: status,
+};
