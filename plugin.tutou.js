91e5e934a6375af9dc286889dc46b161b449397d
hcz <h.cz@qq.com>
2018-03-23 21:58:05 -0400

    add plugin.tutou.js

---
+++ plugin.tutou.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const config = require('./config');
+
+module.exports = (bot, event, playerEvent, env) => {
+    bot.onText(/^\/roll(@\w+)?(?: ((?!_)\w*))?$/, event((msg, match) => {
+        const title = config.tutouGroupTitle[msg.chat.id];
+
+        if (title) {
+            let text = '';
+            let matchIndex = 2;
+
+            for (const i in title) {
+                if (title[i][0] === '/') {
+                    let chosen = null;
+
+                    if (match[matchIndex]) {
+                        chosen = env.command.tryGet(msg, match[matchIndex], [], false);
+
+                        matchIndex += 1;
+                    }
+
+                    if (!chosen) {
+                        chosen = env.command.tryGet(msg, title[i].slice(1), [], false);
+                    }
+
+                    if (!chosen) {
+                        text = '';
+
+                        break;
+                    }
+
+                    text += chosen.text;
+                } else {
+                    text += title[i];
+                }
+            }
+
+            bot.setChatTitle(
+                msg.chat.id,
+                text
+            );
+        }
+    }, 1));
+};
