11e45451d977aaafdf462cfdf6b80dd746362e0c
hcz <h.cz@qq.com>
2018-06-19 23:05:33 -0400

    implement 2 user leave message

--- plugin.welcome.js
+++ plugin.welcome.js
@@ -1,32 +1,33 @@
 'use strict';
 
 module.exports = (bot, event, playerEvent, env) => {
-    const pairs = {};
+    const welcomePairs = {};
+    const leavePairs = {};
+
+    const gen = (command, pairs, msg, user) => {
+        const name1 = '@' + user.username || user.first_name;
+
+        if (pairs[msg.chat.id] && pairs[msg.chat.id] !== name1) {
+            const name2 = pairs[msg.chat.id];
+
+            delete pairs[msg.chat.id];
+
+            env.command.get(msg, command, [name1, name2]);
+        } else {
+            pairs[msg.chat.id] = name1;
+
+            env.command.get(msg, command, [name1]);
+        }
+    };
 
     const welcomeEvent = event((msg, match) => {
         for (const i in msg.new_chat_members) {
-            const name1 = '@' + msg.new_chat_members[i].username
-                || msg.new_chat_members[i].first_name;
-
-            if (pairs[msg.chat.id]) {
-                const name2 = pairs[msg.chat.id];
-
-                delete pairs[msg.chat.id];
-
-                env.command.get(msg, 'welcome', [name2, name1]);
-            } else {
-                pairs[msg.chat.id] = name1;
-
-                env.command.get(msg, 'welcome', [name1]);
-            }
+            gen('welcome', welcomePairs, msg, msg.new_chat_members[i]);
         }
     }, -1);
 
     const leaveEvent = event((msg, match) => {
-        const name1 = '@' + msg.left_chat_member.username
-            || msg.left_chat_member.first_name;
-
-        env.command.get(msg, 'leave', [name1]);
+        gen('leave', leavePairs, msg, msg.left_chat_member);
     }, -1);
 
     bot.on('message', (msg) => {
