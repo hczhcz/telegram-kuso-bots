c7c18a3c7e112230a3ac84384c24e48bd8881528
hcz <h.cz@qq.com>
2018-10-26 00:05:38 -0400

    implement del in plugin.one.js

--- plugin.one.js
+++ plugin.one.js
@@ -11,27 +11,49 @@ const splitter = new GraphemeSplitter();
 module.exports = (bot, event, playerEvent, env) => {
     const chars = [];
 
-    readline.createInterface({
-        input: fs.createReadStream(config.onePathChars),
-    }).on('line', (line) => {
-        const obj = JSON.parse(line);
-
-        chars.push(obj);
-    });
-
-    const fdChars = fs.openSync(config.onePathChars, 'a');
-
-    const addChar = (obj) => {
+    const findChar = (obj) => {
         for (const i in chars) {
             if (
                 obj.text === chars[i].text
                 && obj.from_id === chars[i].from_id
             ) {
-                return;
+                return i;
             }
         }
 
-        chars.push(obj);
+        return null;
+    };
+
+    readline.createInterface({
+        input: fs.createReadStream(config.onePathChars),
+    }).on('line', (line) => {
+        const obj = JSON.parse(line);
+
+        if (obj.del) {
+            chars.splice(findChar(obj), 1);
+        } else {
+            chars.push(obj);
+        }
+    });
+
+    const fdChars = fs.openSync(config.onePathChars, 'a');
+
+    const addChar = (obj) => {
+        const found = findChar(obj);
+
+        if (obj.del) {
+            if (found === null) {
+                return;
+            }
+
+            chars.splice(found, 1);
+        } else {
+            if (found !== null) {
+                return;
+            }
+
+            chars.push(obj);
+        }
 
         fs.write(fdChars, JSON.stringify(obj) + '\n', () => {
             // nothing
@@ -49,6 +71,7 @@ module.exports = (bot, event, playerEvent, env) => {
                 from_id: msg.forward_from.id,
                 chat_id: msg.chat.id,
                 message_id: msg.message_id,
+                del: false,
             });
         } else {
             addChar({
@@ -56,6 +79,7 @@ module.exports = (bot, event, playerEvent, env) => {
                 from_id: msg.from.id,
                 chat_id: msg.chat.id,
                 message_id: msg.message_id,
+                del: false,
             });
         }
     }, -1));
@@ -90,7 +114,11 @@ module.exports = (bot, event, playerEvent, env) => {
                     msg.chat.id,
                     option.chat_id,
                     option.message_id
-                ).then(send);
+                ).catch((err) => {
+                    option.del = true;
+                    addChar(option);
+                    send();
+                }).then(send);
             }
         };
 
