99cadc745beef65f67a5d007714468a60ffe9024
hcz <h.cz@qq.com>
2019-01-19 23:51:50 +0800

    add listre

--- plugin.chinanet.js
+++ plugin.chinanet.js
@@ -3,6 +3,22 @@
 const config = require('./config');
 
 module.exports = (bot, event, playerEvent, env) => {
+    bot.onText(/^\/listre(@\w+)?$/, event((msg, match) => {
+        let text = '';
+
+        for (const i in config.chinanetCommandMap) {
+            text += config.chinanetCommandMap[i] + ' -> ' + i + '\n';
+        }
+
+        bot.sendMessage(
+            msg.chat.id,
+            text,
+            {
+                reply_to_message_id: msg.message_id,
+            }
+        );
+    }, 1));
+
     const genEvent = (key) => {
         return (msg, match) => {
             if (!config.threesomeSilent[msg.chat.id]) {
