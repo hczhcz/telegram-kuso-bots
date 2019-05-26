688d8bb5ef7f9e7b3d03ad77219cf350ca34a5b4
hcz <h.cz@qq.com>
2019-05-26 17:27:25 +0800

    add gameinfo

--- plugin.info.js
+++ plugin.info.js
@@ -13,8 +13,19 @@ module.exports = (bot, event, playerEvent, env) => {
         );
     }, 1));
 
+    bot.onText(/^\/gameinfo(@\w+)?$/, event((msg, match) => {
+        bot.sendMessage(
+            msg.chat.id,
+            util.inspect(env.data.games[msg.chat.id]),
+            {
+                reply_to_message_id: msg.message_id,
+            }
+        );
+    }, 1));
+
     env.info.addPluginHelp(
         'info',
-        '/info 显示当前消息 JSON 数据'
+        '/info 显示当前消息 JSON 数据\n'
+            + '/gameinfo 显示当前啪啪 JSON 数据'
     );
 };
