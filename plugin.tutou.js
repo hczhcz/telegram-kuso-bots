36f720e472ba96be1b4d1a1071bede70f7e97ad9
hcz <h.cz@qq.com>
2020-04-30 01:54:41 +0800

    add webp support in plugin.tutou.js

--- plugin.tutou.js
+++ plugin.tutou.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const canvas = require('canvas');
+const cwebp = require('cwebp');
 
 const config = require('./config');
 
@@ -14,15 +15,62 @@ module.exports = (bot, event, playerEvent, env) => {
 
     bot.onText(/^\/addtutou(@\w+)?(?: (-?[\d.]+) (-?[\d.]+))?(?: (-?)([\d.]+))?(?: (-?\d+))?$/, event((msg, match) => {
         if (msg.reply_to_message) {
-            let file_id = null;
+            const handleImage = (link) => {
+                canvas.loadImage(link).then((bgImage) => {
+                    const image = canvas.createCanvas(bgImage.width, bgImage.height);
+                    const ctx = image.getContext('2d');
 
-            // TODO: webp is not supported
-            // if (msg.reply_to_message.sticker) {
-            //     file_id = msg.reply_to_message.sticker.file_id;
-            // }
+                    ctx.drawImage(bgImage, 0, 0);
+
+                    const left = bgImage.width * (
+                        match[2]
+                            ? Math.min(Math.max(parseFloat(match[2]), -1), 2)
+                            : Math.asin(Math.random() * 2 - 1) / Math.PI + 0.5
+                    );
+                    const top = bgImage.height * (
+                        match[3]
+                            ? Math.min(Math.max(parseFloat(match[3]), -1), 2)
+                            : Math.asin(Math.random() * 2 - 1) / Math.PI + 0.5
+                    );
+                    const size = Math.min(bgImage.width, bgImage.height) * (
+                        match[5]
+                            ? Math.min(Math.max(parseFloat(match[5]), 0), 10)
+                            : Math.random() * 0.8 + 0.1
+                    );
+                    const angle = match[6]
+                        ? Math.min(Math.max(parseInt(match[6], 10), -360), 360) * Math.PI / 180
+                        : Math.asin(Math.random() * 2 - 1) * 2;
+
+                    ctx.translate(left, top);
+                    ctx.rotate(angle);
+
+                    if (match[4] || match[4] !== '' && Math.random() < 0.5) {
+                        ctx.scale(-1, 1);
+                    }
+
+                    ctx.drawImage(tutouImage, -size / 2, -size / 2, size, size);
+
+                    bot.sendPhoto(
+                        msg.chat.id,
+                        image.toBuffer(),
+                        {
+                            reply_to_message_id: msg.message_id,
+                        }
+                    );
+                });
+            };
+
+            if (msg.reply_to_message.sticker) {
+                const decoder = new cwebp.DWebp(bot.getFileStream(msg.reply_to_message.sticker.file_id));
+
+                decoder.toBuffer((err, buffer) => {
+                    handleImage(buffer);
+                });
+            }
 
             if (msg.reply_to_message.photo) {
                 let best_width = 0;
+                let file_id = null;
 
                 for (const i in msg.reply_to_message.photo) {
                     if (best_width < msg.reply_to_message.photo[i].width) {
@@ -30,53 +78,8 @@ module.exports = (bot, event, playerEvent, env) => {
                         file_id = msg.reply_to_message.photo[i].file_id;
                     }
                 }
-            }
 
-            if (file_id) {
-                bot.getFileLink(file_id).then((link) => {
-                    canvas.loadImage(link).then((bgImage) => {
-                        const image = canvas.createCanvas(bgImage.width, bgImage.height);
-                        const ctx = image.getContext('2d');
-
-                        ctx.drawImage(bgImage, 0, 0);
-
-                        const left = bgImage.width * (
-                            match[2]
-                                ? Math.min(Math.max(parseFloat(match[2]), -1), 2)
-                                : Math.asin(Math.random() * 2 - 1) / Math.PI + 0.5
-                        );
-                        const top = bgImage.height * (
-                            match[3]
-                                ? Math.min(Math.max(parseFloat(match[3]), -1), 2)
-                                : Math.asin(Math.random() * 2 - 1) / Math.PI + 0.5
-                        );
-                        const size = Math.min(bgImage.width, bgImage.height) * (
-                            match[5]
-                                ? Math.min(Math.max(parseFloat(match[5]), 0), 10)
-                                : Math.random() * 0.8 + 0.1
-                        );
-                        const angle = match[6]
-                            ? Math.min(Math.max(parseInt(match[6], 10), -360), 360) * Math.PI / 180
-                            : Math.asin(Math.random() * 2 - 1) * 2;
-
-                        ctx.translate(left, top);
-                        ctx.rotate(angle);
-
-                        if (match[4] || match[4] !== '' && Math.random() < 0.5) {
-                            ctx.scale(-1, 1);
-                        }
-
-                        ctx.drawImage(tutouImage, -size / 2, -size / 2, size, size);
-
-                        bot.sendPhoto(
-                            msg.chat.id,
-                            image.toBuffer(),
-                            {
-                                reply_to_message_id: msg.message_id,
-                            }
-                        );
-                    });
-                });
+                bot.getFileLink(file_id).then(handleImage);
             }
         }
     }));
