dd60bdad19b033175b3ab82056b45474152ea6db
hcz <h.cz@qq.com>
2021-05-17 22:14:40 +0800

    add message filter in talk bot

--- talk.js
+++ talk.js
@@ -45,6 +45,10 @@ const updateCorpus = () => {
             const payload = {};
 
             if (obj.text) {
+                if (obj.text.match(/@\w+|\/\w+|:\/\//)) {
+                    return;
+                }
+
                 payload.text = obj.text;
             }
 
