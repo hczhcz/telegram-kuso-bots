ae0ba0ca748a76a986bc9f23ce27ce3cd6effeec
hcz <h.cz@qq.com>
2025-01-26 00:17:18 +0800

    add thinking llm

--- plugin.llm.js
+++ plugin.llm.js
@@ -110,10 +110,104 @@ module.exports = (bot, event, playerEvent, env) => {
         }
     }, 2));
 
+    bot.onText(/^\/aithink(@\w+)?(?: ([^\0]*))$/, event((msg, match) => {
+        const rawMessages = [];
+        const messages = [{
+            role: 'system',
+            content: '你是' + config.threesomeUsername + '推理助手\n'
+                + '你思考并准确回答用户提问',
+        }];
+
+        if (msg.reply_to_message && msg.reply_to_message.text) {
+            rawMessages.push(msg.reply_to_message.text);
+            messages.push({
+                role: 'user',
+                content: msg.reply_to_message.text.slice(0, config.llmLimit),
+            });
+        }
+
+        if (match[2]) {
+            rawMessages.push(match[2]);
+            messages.push({
+                role: 'user',
+                content: match[2].slice(0, config.llmLimit),
+            });
+        }
+
+        if (rawMessages.length) {
+            const req = https.request(config.llmUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + config.llmToken,
+                },
+            }, (res) => {
+                const data = [];
+
+                res.on('data', (chunk) => {
+                    data.push(chunk);
+                });
+
+                res.on('end', () => {
+                    const think = JSON.parse(Buffer.concat(data).toString()).choices[0].message.reasoning_content;
+                    const result = JSON.parse(Buffer.concat(data).toString()).choices[0].message.content;
+
+                    rawMessages.push(think);
+                    rawMessages.push(result);
+                    fs.write(fd, JSON.stringify(rawMessages) + '\n', () => {
+                        // nothing
+                    });
+
+                    bot.sendMessage(
+                        msg.chat.id,
+                        think,
+                        {
+                            parse_mode: 'Markdown',
+                            reply_to_message_id: msg.message_id,
+                        }
+                    ).catch((err) => {
+                        bot.sendMessage(
+                            msg.chat.id,
+                            think,
+                            {
+                                reply_to_message_id: msg.message_id,
+                            }
+                        );
+                    });
+
+                    bot.sendMessage(
+                        msg.chat.id,
+                        result,
+                        {
+                            parse_mode: 'Markdown',
+                            reply_to_message_id: msg.message_id,
+                        }
+                    ).catch((err) => {
+                        bot.sendMessage(
+                            msg.chat.id,
+                            result,
+                            {
+                                reply_to_message_id: msg.message_id,
+                            }
+                        );
+                    });
+                });
+            });
+
+            req.write(JSON.stringify({
+                model: config.llmThinkModel,
+                messages: messages,
+                max_tokens: config.llmLimit,
+            }));
+            req.end();
+        }
+    }, 1));
+
     env.info.addPluginHelp(
         'llm',
         '/ai 向通用人工智能模型提问\n'
             + '/aicat 向猫娘人工智能模型提问\n'
-            + '/aicode 向代码人工智能模型提问'
+            + '/aicode 向代码人工智能模型提问\n'
+            + '/aithink 向推理人工智能模型提问'
     );
 };
